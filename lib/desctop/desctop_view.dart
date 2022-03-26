5508dfcc36fa3d744e66a233ff8c5fc17de82840
rubydenial <kabalamabonomo@gmail.com>
2022-03-26 19:37:42 +0200

    009. Decoraded button

--- lib/desctop/desctop_view.dart
+++ lib/desctop/desctop_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class DesctopView extends StatelessWidget {
   const DesctopView({Key? key}) : super(key: key);
@@ -20,14 +21,41 @@ class DesctopView extends StatelessWidget {
                   Container(
                     height: s.height,
                     width: (s.width - (s.width * 0.2)) * 0.70,
-                    color: Colors.red,
+                    color: Colors.yellow[200],
                     child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        const Text('I\'m a very good programmer !!! :)))'),
-                        const Text('111111111111111111111111111111111'),
-                        const Text('222222222222222222222222222222222'),
-                        ElevatedButton(
-                          child: const Text('Push me'),
+                        Text(
+                          'I\'m a very good programmer !!! :)))',
+                          style: GoogleFonts.montserrat(fontSize: 60),
+                        ),
+                        const SizedBox(height: 37),
+                        const Text(
+                          '222222222222222222222222222222222',
+                          style: TextStyle(fontSize: 24),
+                        ),
+                        const SizedBox(height: 41),
+                        OutlinedButton(
+                          style: OutlinedButton.styleFrom(
+                            shape: const RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20))),
+                            backgroundColor: Colors.red[400],
+                            side: const BorderSide(
+                                width: 1.5, color: Colors.blueAccent),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 5, vertical: 7),
+                            child: Text(
+                              'Push me',
+                              style: TextStyle(
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.amber[200]),
+                            ),
+                          ),
                           onPressed: () {},
                         )
                       ],
